b421a31da2480a8d7d53679a5b277bd7c89b0886
phani-sukhiba <150000789+phani-sukhiba@users.noreply.github.com>
2024-02-10 04:35:58 +0530

    added new features in homepage

--- home.py
+++ home.py
@@ -1,26 +1,3 @@
-# from flask import Flask, render_template, request, jsonify
-# import google.generativeai as genai
-
-# genai.configure(api_key="AIzaSyD52mO4pFXnWHdhFecBERjpTjT24KGIsxM")
-# model = genai.GenerativeModel('gemini-pro')
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def index():
-#     return render_template('home.html')
-
-# @app.route('/process_text', methods=['POST'])
-# def process_text():
-#     input_text = request.form['inputText']
-#     chat = model.start_chat(history=[])
-#     response = chat.send_message(input_text)
-
-#     return jsonify({'answer': response.text})
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
 from flask import Flask, render_template, request, jsonify
 import google.generativeai as genai
 
@@ -31,6 +8,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('homepage.html')
+
+@app.route('/home.html')
+def index1():
     return render_template('home.html')
 
 @app.route('/process_text', methods=['POST'])
